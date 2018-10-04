1abd8acbcdf7866d7a989f25493f3e1d96638772
Thenea <thenea@iMac4.local>
2018-10-04 12:47:04 -0400

    the first version and the first part of my blackjack project

---
+++ assignment_four.py
@@ -0,0 +1,25 @@
+# Senior
+# Thenea Sun
+# Oct. 3rd
+# The program will model a simple hand of blackjack between one player and a dealer, in order to simulate a very
+# simplified hand of blackjack.
+
+import random
+
+
+def draw_a_card():
+    number = random.randint(1, 10)
+    print(number)
+
+
+def card_total():
+    number = 
+    print(number)
+
+
+def main():
+    draw_a_card()
+    card_total()
+
+
+main()
