adecf7397ae80ca5ce8adbf7fc6b15b2b1be8dc9
Thenea <thenea@iMac4.local>
2018-10-04 12:22:06 -0400

    a new version

--- d2_unit_four_warmups.py
+++ d2_unit_four_warmups.py
@@ -1,7 +1,7 @@
 voting_age = 18
 age = int(input("What is your age?"))
 
-if age >= 18:
+if age >= voting_age:
     print("You are old enough to vote.")
 else:
     print("You are not old enough to vote.")
