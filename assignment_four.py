d501c1b8169bd97f1165f163a083073a540e5ef5
Thenea <thenea@iMac4.local>
2018-10-05 14:15:25 -0400

    This is a fully testing version, I am still trying to fix some of the bugs

--- assignment_four.py
+++ assignment_four.py
@@ -9,17 +9,38 @@ import random
 
 def draw_a_card():
     number = random.randint(1, 10)
-    print(number)
-
-
-def card_total():
-    number = 
-    print(number)
+    return number
 
 
 def main():
-    draw_a_card()
-    card_total()
+    card1 = draw_a_card()
+    card2 = draw_a_card()
+    card3 = draw_a_card()
+    card4 = draw_a_card()
+    card5 = draw_a_card()
+# We need to define the cards here first, otherwise it will not print out the number it got
+    users_total = card1 + card2
+    dealers_total = card4 + card5
+    print("You got a", card1, "and a", card2)
+    print("Your total is", users_total)
+    answer = input("Would you like another card? (Please type 'y' or 'n')")
+    if answer == "y":
+        print("Your new card is", card3, "and your total is now", users_total + card3)
+        print("The dealer now have a", card4, "and a", card5)
+        print("The dealer's total is", dealers_total)
+    elif answer == "n":
+        print("The dealer now have a", card4, "and a", card5)
+        print("The dealer's total is", dealers_total)
+    else:
+        print("Please type in either 'y' or 'n', otherwise the program will not run correctly.")
+
+    if users_total + card3 >= 21:
+        print("Your total went over 21, you lose.")
+    elif users_total + card3 > dealers_total:
+        print("You win!")
+    elif users_total + card3 <= dealers_total:
+        print("You lose.")
+# These statements make sure the program runs without misjudgement.
 
 
 main()
