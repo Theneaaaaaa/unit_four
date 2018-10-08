f7a6cbda5cafed9a26caec3f0dfb0d9137ad8d10
Thenea <thenea@iMac4.local>
2018-10-08 15:01:20 -0400

    This is a brand new version of my game, please try to follow the rule XD, if you do not, there will be something intersting for you. Also, I clearfied the whole program.

--- assignment_four.py
+++ assignment_four.py
@@ -10,64 +10,48 @@ import random
 def draw_a_card():
     number = random.randint(1, 10)
     return number
-# The basic function and the only function we really need here XD!
 
 
-def main():
+def game_mode():
     card1 = draw_a_card()
-
     card2 = draw_a_card()
     card3 = draw_a_card()
     card4 = draw_a_card()
     card5 = draw_a_card()
 # We need to define the cards here first, otherwise it will not print out the number it got.
+    users_total = card1 + card2
+    dealers_total = card4 + card5
+    print("You got a", card1, "and a", card2)
+    print("Your total is", users_total)
+    answer = input("Would you like another card? (Please type 'y' or 'n')")
+    if answer == "y":
+        print("Your new card is", card3, "and your total is now", users_total + card3)
+        print("The dealer now have a", card4, "and a", card5)
+        print("The dealer's total is", dealers_total)
+    elif answer == "n":
+        print("The dealer now have a", card4, "and a", card5)
+        print("The dealer's total is", dealers_total)
+    else:
+        print("Please type in either 'y' or 'n', otherwise the program will not run correctly.")
+    win_or_lose = users_total + card3
+    if win_or_lose > 21:
+        print("Your total went over 21, you lose.")
+    elif win_or_lose == 21:
+        print("Blackjack! You win!")
+    elif win_or_lose > dealers_total:
+        print("You win!")
+    else:
+        print("You lose.")
+
+
+def main():
     start_game = input("Welcome to the Blackjack game! I am your dealer. Please type 'start' to play.")
     if start_game == "start":
-        users_total = card1 + card2
-        dealers_total = card4 + card5
-        print("You got a", card1, "and a", card2)
-        print("Your total is", users_total)
-        answer = input("Would you like another card? (Please type 'y' or 'n')")
-        if answer == "y":
-            print("Your new card is", card3, "and your total is now", users_total + card3)
-            print("The dealer now have a", card4, "and a", card5)
-            print("The dealer's total is", dealers_total)
-        elif answer == "n":
-            print("The dealer now have a", card4, "and a", card5)
-            print("The dealer's total is", dealers_total)
-        else:
-            print("Please type in either 'y' or 'n', otherwise the program will not run correctly.")
-
-        if users_total + card3 >= 21:
-            print("Your total went over 21, you lose.")
-        elif users_total + card3 > dealers_total:
-            print("You win!")
-        elif users_total + card3 <= dealers_total:
-            print("You lose.")
+        game_mode()
     else:
         second_chance = input("Please type 'start' to start the game.")
         if second_chance == "start":
-            users_total = card1 + card2
-            dealers_total = card4 + card5
-            print("You got a", card1, "and a", card2)
-            print("Your total is", users_total)
-            answer = input("Would you like another card? (Please type 'y' or 'n')")
-            if answer == "y":
-                print("Your new card is", card3, "and your total is now", users_total + card3)
-                print("The dealer now have a", card4, "and a", card5)
-                print("The dealer's total is", dealers_total)
-            elif answer == "n":
-                print("The dealer now have a", card4, "and a", card5)
-                print("The dealer's total is", dealers_total)
-            else:
-                print("Please type in either 'y' or 'n', otherwise the program will not run correctly.")
-
-            if users_total + card3 >= 21:
-                print("Your total went over 21, you lose.")
-            elif users_total + card3 > dealers_total:
-                print("You win!")
-            elif users_total + card3 <= dealers_total:
-                print("You lose.")
+            game_mode()
         else:
             print("You lost. Because you did not even want to start the game.")
 
